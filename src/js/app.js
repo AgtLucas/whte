133d99fedff83de061e06eeb6a18dcfe69b9d202
Lucas <lucas@agtlucas.com>
2016-01-28 04:07:38 -0200

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,5 @@
 import angular from 'angular'
+import ngResource from 'angular-resource'
+import ngRouter from 'angular-ui-router'
 
-angular.module('movieApp', [])
+angular.module('movieApp', [ngResource, ngRouter])
