04de09dc1eb3c52fc778d98399c90f71cabffef1
Lucas <lucas@agtlucas.com>
2016-01-28 06:21:54 -0200

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -3,19 +3,9 @@ import ngResource from 'angular-resource'
 import ngRouter from 'angular-ui-router'
 import MovieFactory from './services'
 
-const theApp = angular.module('movieApp', [ngResource, ngRouter])
+angular.module('movieApp', [ngResource, ngRouter, 'movieApp.controllers', 'movieApp.services'])
 
-// theApp.factory('Movie', function ($resource) {
-//   return $resource('http://movieapp-sitepointdemos.rhcloud.com/api/movies/:id', { id: '@_id' }, {
-//     update: {
-//       method: 'PUT'
-//     }
-//   })
-// })
-
-theApp.factory('Movie', MovieFactory)
-
-theApp.config(function ($stateProvider) {
+angular.module('movieApp').config(function ($stateProvider) {
   $stateProvider.state('movies', {
     url: '/movies',
     templateUrl: 'src/partials/movies.html',
@@ -36,3 +26,53 @@ theApp.config(function ($stateProvider) {
 }).run(function ($state) {
   $state.go('movies')
 })
+
+// theApp.factory('Movie', function ($resource) {
+//   return $resource('http://movieapp-sitepointdemos.rhcloud.com/api/movies/:id', { id: '@_id' }, {
+//     update: {
+//       method: 'PUT'
+//     }
+//   })
+// })
+
+// Factory
+angular.module('movieApp.services', []).factory('Movie', MovieFactory)
+
+// Controllers
+angular.module('movieApp.controllers', [])
+  .controller('MovieListController', ['$scope', '$state', '$window', 'Movie', function ($scope, $state, $window, Movie) {
+    $scope.movies = Movie.query()
+
+    $scope.deleteMovie = function (movie) {
+      // if (popupService.showPopup('Really delete this?')) {
+        movie.$delete(function () {
+          $window.location.href = ''
+        })
+      // }
+    }
+  }])
+  .controller('MovieViewController', ['$scope', '$stateParams', 'Movie', function ($scope, $stateParams, Movie) {
+    $scope.movie = Movie.get({ id: $stateParams.id })
+  }])
+  .controller('MovieCreateController', ['$scope', '$state', '$stateParams', 'Movie', function ($scope, $state, $stateParams, Movie) {
+    $scope.movie = new Movie()
+
+    $scope.addMovie = function () {
+      $scope.movie.$save(function () {
+        $state.go('movies')
+      })
+    }
+  }])
+  .controller('MovieEditController', ['$scope', '$state', '$stateParams', 'Movie', function ($scope, $state, $stateParams, Movie) {
+    $scope.updateMovie = function () {
+      $scope.movie.$update(function () {
+        $state.go('movies')
+      })
+    }
+
+    $scope.loadMovie = function () {
+      $scope.movie = Movie.get({ id: $stateParams.id })
+    }
+
+    $scope.loadMovie()
+  }])
